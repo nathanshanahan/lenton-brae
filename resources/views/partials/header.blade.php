9cbf2f85e3f268aea7ab5bb620c8156f24c2bedc
Alex Yeap <alex@bone.digital>
2023-07-22 18:48:12 +1000

    [fix] guard against errors when main menu doesn't exist

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,5 +2,7 @@
 $block = 'masthead';
 @endphp
 <header class="{{ $block }}">
-    @php wp_nav_menu() @endphp
+	@if (has_nav_menu('primary_navigation'))
+    	@php wp_nav_menu() @endphp
+	@endif
 </header>
