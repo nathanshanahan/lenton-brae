b3e500ceccc66caaf595b0ef7403ecef52384590
Alex Yeap <alex@bone.digital>
2023-09-04 23:03:50 +0700

    [feat] create minimal theme

---
+++ web/app/themes/skeleton/404.php
@@ -0,0 +1,14 @@
+<?php
+
+get_header();
+?>
+
+	<main class="site-main">
+		<header>
+			<h1 class="page-title">404 - page not found</h1>
+		</header>
+	</main>
+
+<?php
+get_sidebar();
+get_footer();
---
+++ web/app/themes/skeleton/index.php
@@ -0,0 +1,39 @@
+<?php
+
+get_header();
+?>
+
+	<main class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				the_content();
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'partials/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
